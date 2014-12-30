7e449a121259ab01c537111f6a671db69d0cac59
Honza Široký <siroky.honza@gmail.com>
2014-12-31 00:14:43 +0100

    Added missing file

---
+++ src/FuncSharp/DataTypes/Nothing/Nothing.cs
@@ -0,0 +1,13 @@
+﻿namespace FuncSharp
+{
+    /// <summary>
+    /// The bottom uninhabited type. Can be e.g. used to denote return type of methods that never return (always throw an exception).
+    /// </summary>
+    public sealed class Nothing
+    {
+        private Nothing()
+        {
+
+        }
+    }
+}
