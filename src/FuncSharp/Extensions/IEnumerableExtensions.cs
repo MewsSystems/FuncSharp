e854aa56f676f3047fa3dbffed38afadd28687a9
Honza Široký <siroky.honza@gmail.com>
2015-12-06 21:57:08 +0100

    Missing file

---
+++ src/FuncSharp/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncSharp
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IOption<T>> options)
+        {
+            return options.SelectMany(o => o.ToEnumerable());
+        }
+    }
+}
