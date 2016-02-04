86fe4d6e2c9d0f2ddf3f58e1a12680707bf1c87f
Honza Široký <siroky.honza@gmail.com>
2016-02-04 01:14:07 +0100

    Version 0.4.10.6

--- src/FuncSharp/Properties/AssemblyInfo.cs
+++ src/FuncSharp/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("32b29b27-c644-4bda-b2a8-29a8e66fba13")]
 [assembly: AssemblyVersion("0.4.10.5")]
-[assembly: AssemblyFileVersion("0.4.10.5")]
+[assembly: AssemblyFileVersion("0.4.10.6")]
