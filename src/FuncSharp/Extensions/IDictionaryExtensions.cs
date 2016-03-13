d079f86bad788b5f5afd274e105dc107f5d7cd61
Ed Clayton <edc25@cantab.net>
2016-03-13 13:12:44 +0700

    Get for readonly dictionaries. Can't just implement an extension method for IReadOnlyDictionary as it'll cunningly break existing code where we pass a Dictionary (compiler can't choose between the two equally good options; Dictionary implements both interfaces, IDictionary does not implement IReadOnlyDictionary).

--- src/FuncSharp/Extensions/IDictionaryExtensions.cs
+++ src/FuncSharp/Extensions/IDictionaryExtensions.cs
@@ -12,5 +12,22 @@ namespace FuncSharp
             }
             return new Tryer<K, V>(dictionary.TryGetValue).Invoke(key);
         }
+
+        public static IOption<V> Get<K, V>(this IEnumerable<KeyValuePair<K, V>> collection, K key)
+        {
+            if (Equals(key, null))
+            {
+                return Option.Empty<V>();
+            }
+            if (collection is IDictionary<K,V>)
+            {
+                return Get((IDictionary<K,V>)collection, key);
+            }
+            if (collection is IReadOnlyDictionary<K,V>)
+            {
+                return new Tryer<K, V>(((IDictionary<K, V>)collection).TryGetValue).Invoke(key);
+            }
+            return collection.FirstOption(kvp => kvp.Key.Equals(key)).Map(kvp => kvp.Value);
+        }
     }
 }
