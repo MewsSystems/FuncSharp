327716683576db5533122d65539bde6da69f1d69
Honza Široký <siroky.honza@gmail.com>
2017-12-08 13:21:34 +0100

    Version 0.7.1

---
+++ src/FuncSharp.Tests/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace FuncSharp.Tests
+{
+    public class IEnumerableExtensionsTests
+    {
+        [Fact]
+        public void FirstOption()
+        {
+            Assert.True(new List<int>().FirstOption().IsEmpty);
+            Assert.True(new List<int>().LastOption().IsEmpty);
+            Assert.Equal(1.ToOption(), new List<int> { 1, 2, 3 }.FirstOption());
+            Assert.Equal(3.ToOption(), new List<int> { 1, 2, 3 }.LastOption());
+        }
+    }
+}
