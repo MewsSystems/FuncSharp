d70982d28cfc5a1976382b9e6076cc6c8924a1a8
Honza Široký <siroky.honza@gmail.com>
2018-06-03 14:43:49 +0200

    Simplified solution, released 0.10.0.

--- nuget/Push.cmd
+++
@@ -1,2 +0,0 @@
-Nuget Push FuncSharp.*.nupkg -Source https://www.nuget.org/api/v2/package
-del FuncSharp.*.nupkg
---
+++ publish.cmd
@@ -0,0 +1,3 @@
+rem Do not forget to rebuild the solution with Release target.
+
+Nuget Push src/FuncSharp/bin/Release/FuncSharp.0.10.0.nupkg -Source https://www.nuget.org/api/v2/package
