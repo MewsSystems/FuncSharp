1d6d8f701187c1d6745f986943c090e7016bbb93
Kalicz <kalandrapavel@gmail.com>
2021-09-30 10:30:08 +0200

    CR

--- src/FuncSharp.Examples/IOption.cs
+++ src/FuncSharp.Examples/IOption.cs
@@ -18,19 +18,23 @@ namespace FuncSharp.Examples
             IOption<string> stringifiedDivisionResult = divisionResult.Map(r => r.ToString());
         }
 
-        private void HandlingNestedOptionsWithFlatMap(decimal number, decimal divisor)
+        private void HandlingNestedOptionsWithFlatMap(decimal number, decimal firstDivisor, decimal secondDivisor)
         {
-            IOption<decimal> divisionResult = Divide(number, divisor);
-            IOption<IOption<decimal>> resultOfDoubleDivision = divisionResult.Map(r => Divide(r, divisor));
-            IOption<decimal> flattenedResultOfDoubleDivision1 = resultOfDoubleDivision.Flatten(); // This option has value if both the inner and the outer option have value.
+            IOption<decimal> divisionResult = Divide(number, firstDivisor);
+            IOption<IOption<decimal>> resultOfDoubleDivision = divisionResult.Map(r => Divide(r, secondDivisor));
+
+            // This option has value if both the inner and the outer option have value.
+            IOption<decimal> flattenedResultOfDoubleDivision1 = resultOfDoubleDivision.Flatten();
+
             // Same can be done with 1 call.
-            IOption<decimal> flattenedResultOfDoubleDivision2 = divisionResult.FlatMap(r => Divide(r, divisor));
+            IOption<decimal> flattenedResultOfDoubleDivision2 = divisionResult.FlatMap(r => Divide(r, secondDivisor));
         }
 
         private void UsingOptionValueWithMatch(decimal number, decimal divisor)
         {
             var divisionResult = Divide(number, divisor);
-            // This overload takes 2 Func parameters. Each of those have to return a value and result is stored in the `roundedResult` variable.
+
+            // This overload takes 2 Func parameters. Each of those have to return a value and result is stored in the roundedResult variable.
             decimal roundedResultOrFourteen = divisionResult.Match(
                 result => Math.Round(result),
                 _ => 14
@@ -49,7 +53,7 @@ namespace FuncSharp.Examples
         {
             var divisionResult = Divide(number, divisor);
 
-            // `Get` will throw an exception in case of empty option. Therefore you should not really use it at all.
+            // Get method will throw an exception in case of empty option. Using it is an anti-pattern.
             // You should rather use Match to branch your code into individual cases where each case is guaranteed to work.
             decimal valueOrExceptionThrown = divisionResult.Get();
 
@@ -65,14 +69,15 @@ namespace FuncSharp.Examples
             );
         }
 
-        private void HandlingCollectionsOfOptions()
+        private void HandlingCollectionsOfOptions(List<int> numbers, List<int> divisors)
         {
-            var numbers = new List<int> { 0, 1, 2, 3 };
-            var divisors = new List<int> { 0, 1, 2, 3 };
             IEnumerable<IOption<decimal>> divisionResults = numbers.SelectMany(n => divisors.Select(d => Divide(n, d))).ToList();
-            // These two lines produce equal results. But flatten is more readable and generally using `Get` is an anti-pattern as it is not safe.
+
+            // These two lines produce equal results. But flatten is more readable and generally using Get is an anti-pattern as it is not safe.
             IEnumerable<decimal> successfulResults1 = divisionResults.Flatten();
-            IEnumerable<decimal> successfulResults2 = divisionResults.Where(r => r.NonEmpty).Select(r => r.Get()); // get throws exception when called on empty option
+
+            // Get method throws exception when called on empty option
+            IEnumerable<decimal> successfulResults2 = divisionResults.Where(r => r.NonEmpty).Select(r => r.Get());
 
             int errorResultCount = divisionResults.Count(r => r.IsEmpty);
         }
@@ -89,10 +94,19 @@ namespace FuncSharp.Examples
             IOption<bool> valuedOption2 = nullableBool.ToOption();
             IOption<bool> valuedOption3 = Option.Create(nullableBool);
 
-            // Option.Valued can construct options with null value inside.
+            // Option.Valued can construct options with null value inside. Therefore it can cause confusion and is an anti-pattern.
             IOption<bool?> valuedOptionWithNullInside1 = Option.Valued(emptyNullableBool);
             IOption<bool?> valuedOptionWithFalse = Option.Valued(nullableBool);
             IOption<object> valuedOptionWithNullInside2 = Option.Valued<object>(null);
+
+            // Instead, you can use option of an option. For example when updating a value of a nullable property.
+            // Outer option defines whether we're changing value, inner option holds the value to assign.
+            IOption<IOption<bool>> notUpdating = Option.Empty<IOption<bool>>();
+            IOption<IOption<bool>> settingToTrue1 = true.ToOption().ToOption();
+            IOption<IOption<bool>> settingToTrue2 = Option.Create(true.ToOption());
+            IOption<IOption<bool>> settingToTrue3 = Option.Create(Option.Create(true));
+            IOption<IOption<bool>> settingToNull1 = Option.Empty<bool>().ToOption();
+            IOption<IOption<bool>> settingToNull2 = Option.Create(Option.Empty<bool>());
         }
     }
 }
