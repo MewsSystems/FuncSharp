67d4e35b43577b91da9241052cdbd8f0f20762a0
siroky.honza <siroky.honza@gmail.com>
2021-11-26 10:57:38 +0100

    New version.

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 rem Do not forget to update the project properties and rebuild the solution with Release target. Also update the version below.
 
-"C:\Program Files\NuGet\nuget.exe" Push src/FuncSharp/bin/Release/FuncSharp.3.0.3.nupkg -Source https://www.nuget.org/api/v2/package
+"C:\Program Files\NuGet\nuget.exe" Push src/FuncSharp/bin/Release/FuncSharp.4.0.0.nupkg -Source https://www.nuget.org/api/v2/package
