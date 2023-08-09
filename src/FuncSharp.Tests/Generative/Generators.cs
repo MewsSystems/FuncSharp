b666ee88048e299ee0b3c8c6de4d5b43dbc5934a
Pavel Kalandra <kalandrapavel@gmail.com>
2023-08-09 13:37:48 +0200

    Seed of generative testing + tests for Option.Map (#108)
    * WIP
    
    * Implemented generative testing. So far only for Map method.
    
    * Moved shrinkers into a separate file. Also defined a test for Map of double.

---
+++ src/FuncSharp.Tests/Generative/Generators.cs
@@ -0,0 +1,38 @@
+﻿using FsCheck;
+
+namespace FuncSharp.Tests.Generative;
+
+public record class ReferenceType (int Value);
+
+public class Generators
+{
+    static Generators()
+    {
+        BoolOptions = Arb.From(DefaultOptionGenerator<bool>()); // No shrinker needed for booleans
+        IntOptions = Arb.From(DefaultOptionGenerator<int>(), Shrinkers.IntOption);
+        DecimalOptions = Arb.From(DefaultOptionGenerator<decimal>(), Shrinkers.DecimalOption);
+        DoubleOptions = Arb.From(DefaultOptionGenerator<double>(), Shrinkers.DoubleOption);
+
+        UnitOptions = Arb.From(Gen.Constant(Unit.Value).SometimesEmpty()); // No shrinker needed for Unit
+
+        var referenceTypeOptionGenerator = Arb.From<int>().Generator.Select(i => new ReferenceType(i)).SometimesEmpty();
+        ReferenceTypeOptions = Arb.From(referenceTypeOptionGenerator, Shrinkers.ReferenceTypeOption);
+    }
+
+    public static Arbitrary<IOption<bool>> BoolOptions { get; }
+
+    public static Arbitrary<IOption<int>> IntOptions { get; }
+
+    public static Arbitrary<IOption<decimal>> DecimalOptions { get; }
+
+    public static Arbitrary<IOption<double>> DoubleOptions { get; }
+
+    public static Arbitrary<IOption<Unit>> UnitOptions { get; }
+
+    public static Arbitrary<IOption<ReferenceType>> ReferenceTypeOptions { get; }
+
+    private static Gen<IOption<T>> DefaultOptionGenerator<T>()
+    {
+        return Arb.From<T>().Generator.SometimesEmpty();
+    }
+}
---
+++ src/FuncSharp.Tests/Options/MapTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using FsCheck;
+using FsCheck.Xunit;
+using FuncSharp.Tests.Generative;
+using Xunit;
+
+namespace FuncSharp.Tests.Options
+{
+    public class MapTests
+    {
+        static MapTests()
+        {
+            Arb.Register<Generators>();
+        }
+
+        [Fact]
+        internal void Map()
+        {
+            // Empty option mapped is always empty
+            OptionAssert.IsEmpty(Option.Empty<int>().Map(v => v * 2));
+            OptionAssert.IsEmpty(Option.Empty<string>().Map(v => (int?)null));
+
+            // Valued option mapped will always have value
+            OptionAssert.HasValue(84, 42.ToOption().Map(v => v * 2));
+            OptionAssert.HasValue(84, 42.ToOption().Map(v => v * 2 as int?));
+            OptionAssert.HasValue("xxxxx", 5.ToOption().Map(v => new String('x', v)));
+
+            // Even if you map to null, the option is still valued
+            OptionAssert.NonEmpty(5.ToOption().Map(v => null as int?));
+            OptionAssert.NonEmpty(5.ToOption().Map(v => (string)null));
+        }
+
+        [Property]
+        internal void Map_int(IOption<int> option)
+        {
+            AssertMapResult(option, i => i * 2);
+        }
+
+        [Property]
+        internal void Map_decimal(IOption<decimal> option)
+        {
+            AssertMapResult(option, d => d * 2);
+        }
+
+        [Property]
+        internal void Map_double(IOption<double> option)
+        {
+            AssertMapResult(option, d => d * 2);
+        }
+
+        [Property]
+        internal void Map_bool(IOption<bool> option)
+        {
+            AssertMapResult(option, b => !b);
+        }
+
+        [Property]
+        internal void Map_ReferenceType(IOption<ReferenceType> option)
+        {
+            AssertMapResult(option, d => new ReferenceType(d.Value * 2));
+        }
+
+        private void AssertMapResult<T>(IOption<T> option, Func<T, T> map)
+        {
+            var result = option.Map(map);
+            Assert.Equal(option.IsEmpty, result.IsEmpty);
+            if (option.NonEmpty)
+            {
+                Assert.Equal(map(option.GetOrDefault()), result.GetOrDefault());
+            }
+        }
+    }
+}
