a0b6f45eb0449e2135bd82f30f33b0004557981a
Pavel Kalandra <kalandrapavel@gmail.com>
2023-08-17 09:35:18 +0200

    Moved some extensions and types to FuncSharp. (#119)

---
+++ src/FuncSharp/Numeric/NonPositiveDecimal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace FuncSharp;
+
+public struct NonPositiveDecimal
+{
+    public static readonly NonPositiveDecimal Zero = new(0m);
+
+    private NonPositiveDecimal(decimal value)
+    {
+        Value = value;
+    }
+
+    public decimal Value { get; }
+
+    public static implicit operator decimal(NonPositiveDecimal d)
+    {
+        return d.Value;
+    }
+
+    public static NonPositiveDecimal operator +(NonPositiveDecimal d1, NonPositiveDecimal d2)
+    {
+        return new(d1.Value + d2.Value);
+    }
+
+    public static IOption<NonPositiveDecimal> Create(decimal value)
+    {
+        return CreateNullable(value).ToOption();
+    }
+
+    public static NonPositiveDecimal CreateUnsafe(decimal value)
+    {
+        return CreateNullable(value) ?? throw new ArgumentException($"'{value}' is not a non-positive decimal.");
+    }
+
+    public static NonPositiveDecimal? CreateNullable(decimal value)
+    {
+        return value <= 0 ? new(value) : null;
+    }
+
+    public NonPositiveDecimal Sum(params NonPositiveDecimal[] values)
+    {
+        return new(values.Aggregate(Value, (d1, d2) => d1 + d2));
+    }
+
+    public NonPositiveDecimal Min(NonPositiveDecimal other)
+    {
+        return new(Math.Min(Value, other.Value));
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
