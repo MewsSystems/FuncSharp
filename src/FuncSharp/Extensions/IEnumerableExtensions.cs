14b963283f39429485151463e7d7673b14e5626f
KaliCZ <kalandrapavel@gmail.com>
2023-08-17 08:03:25 +0200

    Returns a proper readonly list.

--- src/FuncSharp/Extensions/IEnumerableExtensions.cs
+++ src/FuncSharp/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace FuncSharp
         /// </summary>
         public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> e)
         {
-            return e.ToList();
+            return e.ToList().AsReadOnly();
         }
 
         /// <summary>
