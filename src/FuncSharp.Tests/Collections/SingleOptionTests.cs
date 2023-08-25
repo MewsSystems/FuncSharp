86193e6567e124ecd1a6a7dfbde703312f22408e
KaliCZ <kalandrapavel@gmail.com>
2023-08-25 13:45:19 +0200

    Tests for FirstOption, LastOption, also improvements to SingleOption tests.

--- src/FuncSharp.Tests/Collections/SingleOptionTests.cs
+++ src/FuncSharp.Tests/Collections/SingleOptionTests.cs
@@ -9,31 +9,49 @@ namespace FuncSharp.Tests.Collections.INonEmptyEnumerable
         [Fact]
         public void SingleOption_Empty()
         {
-            IEnumerable<string> enumerableEmpty = Enumerable.Empty<string>();
-            string[] arrayEmpty = new string[]{};
+            IEnumerable<string> enumerable = Enumerable.Empty<string>();
+            string[] array = new string[]{};
 
-            OptionAssert.IsEmpty(enumerableEmpty.SingleOption());
-            OptionAssert.IsEmpty(arrayEmpty.SingleOption());
+            OptionAssert.IsEmpty(enumerable.SingleOption());
+            OptionAssert.IsEmpty(array.SingleOption());
+
+            OptionAssert.IsEmpty(enumerable.SingleOption(t => t.Contains("potato")));
+            OptionAssert.IsEmpty(array.SingleOption(t => t.Contains("potato")));
         }
 
         [Fact]
         public void SingleOption_Single()
         {
-            IEnumerable<string> enumerableSingle = Enumerable.Repeat("A potato", 1);
-            string[] arraySingle = new []{"A potato"};
+            IEnumerable<string> enumerable = Enumerable.Repeat("A potato", 1);
+            string[] array = new []{"A potato"};
 
-            OptionAssert.NonEmptyWithValue("A potato", enumerableSingle.SingleOption());
-            OptionAssert.NonEmptyWithValue("A potato", arraySingle.SingleOption());
+            OptionAssert.NonEmptyWithValue("A potato", enumerable.SingleOption());
+            OptionAssert.NonEmptyWithValue("A potato", array.SingleOption());
+
+            OptionAssert.NonEmptyWithValue("A potato", enumerable.SingleOption(t => t.Contains("potato")));
+            OptionAssert.NonEmptyWithValue("A potato", array.SingleOption(t => t.Contains("potato")));
+
+            OptionAssert.IsEmpty(enumerable.SingleOption(t => t.Contains("ASDF")));
+            OptionAssert.IsEmpty(array.SingleOption(t => t.Contains("ASDF")));
         }
 
         [Fact]
         public void SingleOption_Multiple()
         {
-            IEnumerable<string> enumerableMultiple = Enumerable.Range(0, 10).Select(i => $"{i} potatoes");
-            string[] arrayMultiple = Enumerable.Range(0, 10).Select(i => $"{i} potatoes").ToArray();
+            IEnumerable<string> enumerable = Enumerable.Range(0, 10).Select(i => $"{i} potatoes");
+            string[] array = Enumerable.Range(0, 10).Select(i => $"{i} potatoes").ToArray();
 
-            OptionAssert.IsEmpty(enumerableMultiple.SingleOption());
-            OptionAssert.IsEmpty(arrayMultiple.SingleOption());
+            OptionAssert.IsEmpty(enumerable.SingleOption());
+            OptionAssert.IsEmpty(array.SingleOption());
+
+            OptionAssert.NonEmptyWithValue("3 potatoes", enumerable.SingleOption(t => t.Contains("3 pot")));
+            OptionAssert.NonEmptyWithValue("3 potatoes", array.SingleOption(t => t.Contains("3 pot")));
+
+            OptionAssert.IsEmpty(enumerable.SingleOption(t => t.Contains("potato")));
+            OptionAssert.IsEmpty(array.SingleOption(t => t.Contains("potato")));
+
+            OptionAssert.IsEmpty(enumerable.SingleOption(t => t.Contains("ASDF")));
+            OptionAssert.IsEmpty(array.SingleOption(t => t.Contains("ASDF")));
         }
     }
 }
