5a7a14a7f29ba58f00f9a231d3b39c7c67418f15
KaliCZ <kalandrapavel@gmail.com>
2023-08-25 13:18:20 +0200

    Implemented tests for AsNonEMpty

--- src/FuncSharp.Tests/Collections/ExceptionsAggregateTests.cs
+++ src/FuncSharp.Tests/Collections/ExceptionsAggregateTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using FsCheck;
 using Xunit;
 
 namespace FuncSharp.Tests.Collections.INonEmptyEnumerable
@@ -31,7 +30,7 @@ namespace FuncSharp.Tests.Collections.INonEmptyEnumerable
             var singleException = new CustomException("A potato here.");
 
             IEnumerable<Exception> enumerable = Enumerable.Repeat(singleException, 1);
-            Exception[] array = new []{singleException};
+            CustomException[] array = new []{singleException};
             var nonEmpty = array.AsNonEmpty().Get();
 
             OptionAssert.NonEmptyWithValue(singleException, enumerable.Aggregate());
@@ -43,8 +42,8 @@ namespace FuncSharp.Tests.Collections.INonEmptyEnumerable
         public void Aggregate_Multiple()
         {
             IEnumerable<Exception> enumerable = Enumerable.Range(0, 10).Select(i => new CustomException($"{i} potatoes"));
-            Exception[] array = Enumerable.Range(0, 10).Select(i => new CustomException($"{i} potatoes")).ToArray();
-            var nonEmpty = array.AsNonEmpty().Get();
+            CustomException[] array = Enumerable.Range(0, 10).Select(i => new CustomException($"{i} potatoes")).ToArray();
+            INonEmptyEnumerable<Exception> nonEmpty = array.AsNonEmpty().Get();
 
             OptionAssert.NonEmptyWithValue(new AggregateException(array), enumerable.Aggregate());
             OptionAssert.NonEmptyWithValue(new AggregateException(array), array.Aggregate());
