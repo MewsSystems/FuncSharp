4249e983d7d19a6a5c222aa1640ad1d310682405
KaliCZ <kalandrapavel@gmail.com>
2023-08-26 02:06:20 +0200

    Stored the benchmark result.

--- src/FuncSharp.Benchmarks/Dictionary/DictionaryBenchmarks.cs
+++ src/FuncSharp.Benchmarks/Dictionary/DictionaryBenchmarks.cs
@@ -15,13 +15,14 @@ namespace FuncSharp.Benchmarks
             Dictionary = Enumerable.Range(0, 1000).ToDictionary(i => i, i => $"{i} potatoes");
         }
 
-        // Last Result - 26.8.2023 -  ns -  B
+        // Last Result - 26.8.2023 - 8.4 ns - 32 B
         [Benchmark]
         public void Get_Valid()
         {
             var x = Dictionary.Get(14);
         }
 
+        // Last Result - 26.8.2023 - 4.7 ns - 0 B
         [Benchmark]
         public void Get_Invalid()
         {
