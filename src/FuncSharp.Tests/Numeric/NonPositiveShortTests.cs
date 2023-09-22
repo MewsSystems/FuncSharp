22f7191fb49b03896c8e2110f6f4186e1270f1a3
KaliCZ <kalandrapavel@gmail.com>
2023-09-22 13:55:32 +0200

    Added extra numeric types.

---
+++ src/FuncSharp.Tests/Numeric/NonPositiveShortTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace FuncSharp.Tests.Numeric;
+
+public class NonPositiveShortTests
+{
+    [Fact]
+    internal void AsNonPositive_Manual()
+    {
+        OptionAssert.IsEmpty(((short)14).AsNonPositive());
+        OptionAssert.IsEmpty(((short)1).AsNonPositive());
+
+        Assert.Equal(0, ((short)0).AsNonPositive().Get());
+        Assert.Equal(-1, ((short)-1).AsNonPositive().Get());
+        Assert.Equal(-20, ((short)-20).AsNonPositive().Get());
+        Assert.Equal(-26579, ((short)-26579).AsNonPositive().Get());
+    }
+
+    [Fact]
+    internal void AsNonPositiveUnsafe_Manual()
+    {
+        Assert.Throws<ArgumentException>(() => ((short)14).AsUnsafeNonPositive());
+        Assert.Throws<ArgumentException>(() => ((short)1).AsUnsafeNonPositive());
+
+        Assert.Equal(0, ((short)0).AsUnsafeNonPositive());
+        Assert.Equal(-1, ((short)-1).AsUnsafeNonPositive());
+        Assert.Equal(-20, ((short)-20).AsUnsafeNonPositive());
+        Assert.Equal(-26579, ((short)-26579).AsUnsafeNonPositive());
+    }
+
+    [Property]
+    internal void AsNonPositive(short number)
+    {
+        var result = number.AsNonPositive();
+        if (number <= 0)
+        {
+            OptionAssert.NonEmpty(result);
+            Assert.Equal(number, result.Get());
+            Assert.Equal(number, result.Get().Value);
+        }
+        else
+        {
+            OptionAssert.IsEmpty(result);
+        }
+    }
+
+    [Property]
+    internal void AsUnsafeNonPositive(short number)
+    {
+        if (number <= 0)
+        {
+            var result = number.AsUnsafeNonPositive();
+            Assert.Equal(number, result);
+            Assert.Equal(number, result.Value);
+        }
+        else
+        {
+            Assert.Throws<ArgumentException>(() => number.AsUnsafeNonPositive());
+        }
+    }
+}
---
+++ src/FuncSharp/Numeric/NonPositiveShort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace FuncSharp;
+
+public struct NonPositiveShort
+{
+    public static readonly NonPositiveShort Zero = new(0);
+
+    private NonPositiveShort(short value)
+    {
+        Value = value;
+    }
+
+    public short Value { get; }
+
+    public static implicit operator short(NonPositiveShort i)
+    {
+        return i.Value;
+    }
+
+    public static NonPositiveShort operator +(NonPositiveShort a, NonPositiveShort b)
+    {
+        return a.Sum(b);
+    }
+
+    public static Option<NonPositiveShort> Create(short value)
+    {
+        return CreateNullable(value).ToOption();
+    }
+
+    public static NonPositiveShort CreateUnsafe(short value)
+    {
+        return CreateNullable(value) ?? throw new ArgumentException($"'{value}' is not a non-positive short.");
+    }
+
+    public static NonPositiveShort? CreateNullable(short value)
+    {
+        return value <= 0 ? new NonPositiveShort(value) : null;
+    }
+
+    public NonPositiveShort Sum(params NonPositiveShort[] values)
+    {
+        return new NonPositiveShort(values.Aggregate(Value, (a, b) => (short)(a + b)));
+    }
+
+    public NonPositiveShort Min(NonPositiveShort other)
+    {
+        return new(Math.Min(Value, other.Value));
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
