c3ed3dd893d461763bb2a1169b2b68d9ac331d18
KaliCZ <kalandrapavel@gmail.com>
2023-09-22 15:30:32 +0200

    Partitioning general enumerables or tries.

---
+++ src/FuncSharp/Collections/IEnumerableExtensions_Partition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncSharp;
+
+public static partial class IEnumerableExtensions
+{
+    /// <summary>
+    /// Splits the items in a collection into 2 collection based on the condition provided.
+    /// </summary>
+    public static (IReadOnlyList<T> Passing, IReadOnlyList<T> Violating) Partition<T>(this IEnumerable<T> e, Func<T, bool> predicate)
+    {
+        var passing = new List<T>();
+        var violating = new List<T>();
+        foreach (var i in e)
+        {
+            if (predicate(i))
+            {
+                passing.Add(i);
+            }
+            else
+            {
+                violating.Add(i);
+            }
+        }
+        return (passing, violating);
+    }
+}
---
+++ src/FuncSharp/Collections/IEnumerableExtensions_Try.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncSharp;
+
+public static partial class IEnumerableExtensions
+{
+    /// <summary>
+    /// Splits a collection of tries into a collection of success results and a collection of errors.
+    /// </summary>
+    public static (IReadOnlyList<TSuccess>, IReadOnlyList<TError>) Partition<TSuccess, TError>(this IEnumerable<Try<TSuccess, TError>> values)
+    {
+        var passing = new List<TSuccess>();
+        var violating = new List<TError>();
+        foreach (var value in values)
+        {
+            if (value.IsSuccess)
+            {
+                passing.Add(value.Success.GetOrDefault());
+            }
+            else
+            {
+                violating.Add(value.Error.GetOrDefault());
+            }
+        }
+        return (passing, violating);
+    }
+
+    /// <summary>
+    /// Splits a collection of tries into a collection of success results and a collection of errors and executes an action for those.
+    /// </summary>
+    public static void PartitionMatch<TSuccess, TError>(this IEnumerable<Try<TSuccess, TError>> source, Action<IReadOnlyList<TSuccess>> f1, Action<IReadOnlyList<TError>> f2)
+    {
+        var list1 = new List<TSuccess>();
+        var list2 = new List<TError>();
+        foreach (var item in source)
+        {
+            item.Match(
+                c1 => list1.Add(c1),
+                c2 => list2.Add(c2)
+            );
+        }
+
+        f1(list1);
+        f2(list2);
+    }
+
+    public static IReadOnlyList<TResult> PartitionMatch<TSuccess, TError, TResult>(this IEnumerable<Try<TSuccess, TError>> source, Func<IReadOnlyList<TSuccess>, IEnumerable<TResult>> f1, Func<IReadOnlyList<TError>, IEnumerable<TResult>> f2)
+    {
+        var list1 = new List<TSuccess>();
+        var list2 = new List<TError>();
+        foreach (var item in source)
+        {
+            item.Match(
+                c1 => list1.Add(c1),
+                c2 => list2.Add(c2)
+            );
+        }
+
+        return ReadOnlyList.CreateFlat(f1(list1), f2(list2));
+    }
+}
