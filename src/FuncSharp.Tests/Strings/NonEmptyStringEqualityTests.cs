1d49a96f6772a62c349b0ab4e7a557fc11a66506
KaliCZ <kalandrapavel@gmail.com>
2024-01-04 10:19:34 +0100

    Improved equality of nonEmptyStrings as well.

--- src/FuncSharp.Tests/Strings/NonEmptyStringEqualityTests.cs
+++ src/FuncSharp.Tests/Strings/NonEmptyStringEqualityTests.cs
@@ -43,21 +43,40 @@ public class NonEmptyStringTests
 
         string text = "ASDF123";
         NonEmptyString nonEmptyString = NonEmptyString.CreateUnsafe("ASDF123");
+        NonEmptyString nonEmptyStringWithSameValue = NonEmptyString.CreateUnsafe("ASDF123");
         Assert.True(text == nonEmptyString);
+        Assert.False(text != nonEmptyString);
         Assert.True(nonEmptyString == text);
+        Assert.False(nonEmptyString != text);
+        Assert.True(nonEmptyString == nonEmptyStringWithSameValue);
+        Assert.False(nonEmptyString != nonEmptyStringWithSameValue);
+
         Assert.True(text.Equals(nonEmptyString));
         Assert.True(nonEmptyString.Equals(text));
+        Assert.True(nonEmptyString.Equals(nonEmptyStringWithSameValue));
+
         Assert.False(object.Equals(text, nonEmptyString)); // Unfortunately string doesn't override the default Equals method to compare with IEquatable<string> therefore this is false.
         Assert.True(object.Equals(nonEmptyString, text));
+        Assert.True(object.Equals(nonEmptyString, nonEmptyStringWithSameValue));
 
         string differentString = "Text14";
         NonEmptyString differentNonEmptyString = NonEmptyString.CreateUnsafe("Totally different text here.");
-        Assert.False(differentNonEmptyString.Equals(differentString));
-        Assert.False(differentString.Equals(differentNonEmptyString));
-        Assert.False(differentNonEmptyString == differentString);
+        NonEmptyString differentNonEmptyString2 = NonEmptyString.CreateUnsafe("And completely different again.");
+
         Assert.False(differentString == differentNonEmptyString);
-        Assert.False(object.Equals(differentNonEmptyString, differentString));
+        Assert.True(differentString != differentNonEmptyString);
+        Assert.False(differentNonEmptyString == differentString);
+        Assert.True(differentNonEmptyString != differentString);
+        Assert.False(differentNonEmptyString == differentNonEmptyString2);
+        Assert.True(differentNonEmptyString != differentNonEmptyString2);
+
+        Assert.False(differentString.Equals(differentNonEmptyString));
+        Assert.False(differentNonEmptyString.Equals(differentString));
+        Assert.False(differentNonEmptyString.Equals(differentNonEmptyString2));
+
         Assert.False(object.Equals(differentString, differentNonEmptyString));
+        Assert.False(object.Equals(differentNonEmptyString, differentString));
+        Assert.False(object.Equals(differentNonEmptyString, differentNonEmptyString2));
 
         NonEmptyString null1 = null;
         string null2 = null;
